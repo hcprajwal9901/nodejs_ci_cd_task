b669a32f72fa65fc8de49bb0904b2f627c696f40
H C Prajwal <hcprajwal9901@gmail.com>
2025-09-23 13:27:01 +0530

    add github actions workflow for git hub pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nodejs_ci_cd_task/',  // ⚠️ use your repo name
-})
+  plugins: [react()],
+  base: '/your-repo-name/', // <--- IMPORTANT: Change 'your-repo-name' to your actual repo name!
+  // Example: If your repo is named 'my-portfolio-app', it would be '/my-portfolio-app/'
+});
