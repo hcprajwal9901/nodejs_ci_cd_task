8e4f157e2d9750aba31f0218d0d874f029e73b26
H C Prajwal <hcprajwal9901@gmail.com>
2025-09-23 14:39:38 +0530

    resolved issues with case sensitivity2

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import app from './app';
+import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
@@ -11,7 +11,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <app/>
+    <App/>
   </React.StrictMode>
 );
 
