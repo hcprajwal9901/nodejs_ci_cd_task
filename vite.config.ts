9f997ce135bd5ba5d111a6f164d21fcc51b3ce7c
H C Prajwal <hcprajwal9901@gmail.com>
2025-09-23 14:22:08 +0530

    resolved issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // <--- IMPORTANT: Change 'your-repo-name' to your actual repo name!
+  base: '/nodejs_ci_cd_task/', // <--- IMPORTANT: Change 'your-repo-name' to your actual repo name!
   // Example: If your repo is named 'my-portfolio-app', it would be '/my-portfolio-app/'
 });
